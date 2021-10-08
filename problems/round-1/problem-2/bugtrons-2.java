7f859f4ef55793b2e562604727f0e94b4d55945b
Anshumaan Kumar Prasad <75872316+amino19@users.noreply.github.com>
2021-10-09 01:08:57 +0530

    Create bugtrons-2.java

---
+++ problems/round-1/problem-2/bugtrons-2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class bugtrons2
+{
+  static int maxProfitofBitcoin(int rate[], int begin, int over)
+{
+
+	// Write your code here
+
+}
+
+// Driver code
+public static void main(String[] args)
+{
+	int rate[] = { 130, 210, 290, 340, 70, 565, 625 };
+	int n = rate.length;
+	System.out.print(maxProfitofBitcoin(rate, 0, n - 1));
+}
+}
