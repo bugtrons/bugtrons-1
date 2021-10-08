ff6e4b35dd7d62565e8d3998f529af573fe649b6
Anshumaan Kumar Prasad <75872316+amino19@users.noreply.github.com>
2021-10-09 01:26:22 +0530

    Create bugtrons-2.cpp

---
+++ solutions/round-2/bugtrons-2.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxProfitofBitcoin(int rate[], int start, int end)
+{
+
+    // If the stocks can't be bought
+    if (end <= start)
+        return 0;
+
+    // Initialise the profit
+    int profit = 0;
+
+    // The day at which the stock
+    // must be bought
+    for (int i = start; i < end; i++) {
+
+        // The day at which the
+        // stock must be sold
+        for (int j = i + 1; j <= end; j++) {
+
+            // If buying the stock at ith day and
+            // selling it at jth day is profitable
+            if (rate[j] > rate[i]) {
+
+                // Update the current profit
+                int curr_profit = rate[j] - rate[i]
+                                + maxProfitofBitcoin(rate, start, i - 1)
+                                + maxProfitofBitcoin(rate, j + 1, end);
+
+                // Update the maximum profit so far
+                profit = max(profit, curr_profit);
+            }
+        }
+    }
+    return profit;
+}
+
+// Driver code
+int main()
+{
+    int rate[] = { 130, 210, 290, 340,
+                    70, 565, 625 };
+    int n = sizeof(rate) / sizeof(rate[0]);
+
+    cout << maxProfitofBitcoin(rate, 0, n - 1);
+
+    return 0;
+}
