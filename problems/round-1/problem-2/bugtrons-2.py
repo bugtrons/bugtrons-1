41314bbb038ff306faaed73f79d6572be912f16d
Anshumaan Kumar Prasad <75872316+amino19@users.noreply.github.com>
2021-10-09 01:15:10 +0530

    Create bugtrons-2.py

---
+++ problems/round-1/problem-2/bugtrons-2.py
@@ -0,0 +1,10 @@
+def maxProfitofBitcoin(rate, begin, over):
+
+	# Write your code here
+
+# Driver code
+if __name__ == '__main__':
+	rate = [ 130, 210, 290, 340, 70, 565, 625];
+	n = len(rate);
+
+	print(maxProfitofBitcoin(rate, 0, n - 1));
